a4e4b8884f7262d367b0815da7e63c66fffe8517
Hafizh <Hafizh@gmail.com>
2024-03-05 14:16:07 +0700

    Delete yellow snack again

--- lib/presentation/orders/pages/order_detail_page.dart
+++ lib/presentation/orders/pages/order_detail_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_local_variable
+// ignore_for_file: unused_local_variable, unused_import
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
